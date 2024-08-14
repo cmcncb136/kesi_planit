dd77111a0667d155e9c8ff2ebf8d235539a30f8d
KJW <cmcncb13624@gmail.com>
2024-08-14 12:34:05 +0900

    feat. Schedule.java Schedule.java Domain 클래스 설계

--- src/main/java/com/kesi/planit/schedule/domain/Schedule.java
+++ src/main/java/com/kesi/planit/schedule/domain/Schedule.java
@@ -1,6 +1,31 @@
 package com.kesi.planit.schedule.domain;
 
+import com.kesi.planit.calendar.domain.Calendar;
+
+import java.awt.*;
+import java.lang.reflect.Member;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
 public class Schedule {
-    public void createAlarm(){}
+    private Member maker;
+    private List<Calendar> calendars; //참석자
+    private Color color;
+
+    private String title;
+    private String description;
+
+    private boolean guestsCanModify;
+
+    private LocalDate startDate, endDate;  // 종일인 경우
+    private LocalDateTime startTime, endTime; //종일이 아닌 경우
+
+    public void createAlarm(){
+
+    }
+
+    //Todo. CRUD 설계
+
 
 }
