7bf244dffc9e781836ff60d2ee2b0c69455e4254
KJW <cmcncb13624@gmail.com>
2024-12-31 19:12:59 +0900

    feat. User Service 테스트 코드 작성

---
+++ src/test/java/com/kesi/planit/user/JoinServiceTest.java
@@ -0,0 +1,85 @@
+package com.kesi.planit.user;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
+import com.google.firebase.auth.UserRecord;
+import com.kesi.planit.calendar.application.CalendarService;
+import com.kesi.planit.calendar.domain.Calendar;
+import com.kesi.planit.core.CommonResult;
+import com.kesi.planit.user.application.JoinService;
+import com.kesi.planit.user.application.UserService;
+import com.kesi.planit.user.domain.User;
+import com.kesi.planit.user.presentation.dto.UserJoinRequestDto;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.time.LocalDate;
+
+import static org.assertj.core.api.Assertions.anyOf;
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+@ExtendWith(SpringExtension.class)
+public class JoinServiceTest {
+    JoinService joinService;
+
+    @MockBean
+    UserService userService;
+
+    @MockBean
+    CalendarService calendarService;
+
+    @MockBean
+    FirebaseAuth auth;
+
+    @MockBean //체인으로 연결된 함수에 경우 체인에서 반환되는 가짜 객체도 필요함
+    UserRecord userRecord;
+
+    @BeforeEach
+    public void setup(){
+        joinService = new JoinService(userService, calendarService, auth);
+    }
+
+    @Test
+    @DisplayName("유저 회원가입")
+    public void join() throws FirebaseAuthException {
+        //given
+        String uid = "x";
+
+        Calendar calendar = Calendar.builder().build();
+        Calendar savedCalendar = Calendar.builder().id(1L).build();
+
+        UserJoinRequestDto joinRequestDto = UserJoinRequestDto.builder()
+                .birth(LocalDate.of(2001,11,11).toString())
+                .gender("Male")
+                .nickname("kim")
+                .build();
+
+        User user = joinRequestDto.toModel(uid, "x@naver.com", "basic.jpg", LocalDate.now(), calendar);
+
+        //Calender, User와 같이 함수 매개변수로 들어가는 객체가 테스트 하려고하는 함수 내부에서 생성된다면
+        //특정 클래스 형태와 맞다면 다음 값을 반환하다라는 식으로 고친다.
+        Mockito.when(calendarService.save(Mockito.any(Calendar.class))).thenReturn(savedCalendar);
+        Mockito.when(userService.save(Mockito.any(User.class))).thenReturn(user);
+
+
+        //Mockito.when(auth.getUser(uid).getEmail()).thenReturn("x@naver.com");
+        // 위와 같이 체인 형태에 경우(중간에 반환되는 객체게 일반적인 객체가 아니라면)
+        // 중간에 반환되는 가짜 객체를 만들고 끊어서  작성해야 한다.
+        //userRecord 가짝 객체의 특정 함수가 실행될 때 어떤 값을 반환할지 설정한다.
+        Mockito.when(userRecord.getEmail()).thenReturn("x@naver.com");
+        Mockito.when(auth.getUser(uid)).thenReturn(userRecord);
+
+        //when
+        CommonResult commonResult = joinService.join(uid, joinRequestDto);
+
+        //then
+        assertThat(commonResult.getCode()).isEqualTo(200);
+    }
+
+}
---
+++ src/test/java/com/kesi/planit/user/UserServiceTest.java
@@ -0,0 +1,213 @@
+package com.kesi.planit.user;
+
+import com.kesi.planit.calendar.application.CalendarService;
+import com.kesi.planit.calendar.domain.Calendar;
+import com.kesi.planit.group.application.repository.GroupAndUserRepo;
+import com.kesi.planit.group.infrastructure.GroupAndUserJpaEntity;
+import com.kesi.planit.user.application.UserService;
+import com.kesi.planit.user.application.repository.UserRepo;
+import com.kesi.planit.user.domain.User;
+import com.kesi.planit.user.infrastructure.UserJpaEntity;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@ExtendWith(SpringExtension.class) //가짜 객체를 사용하기 위해서 사용
+public class UserServiceTest {
+    //test 주체 객체
+    UserService userService;
+
+    @MockBean
+    UserRepo userRepo;
+
+    @MockBean
+    CalendarService calendarService;
+
+    @MockBean
+    GroupAndUserRepo groupAndUserRepo;
+
+    @BeforeEach
+    void setUp() {
+        userService =
+                new UserService(userRepo, groupAndUserRepo, calendarService);
+    }
+
+    @Test
+    @DisplayName("유저 아이디 조회")
+    void getUserById(){
+        //given
+        UserJpaEntity userJpaEntity = UserJpaEntity.builder()
+                .birth(LocalDate.of(2001, 11, 11))
+                .calendarId(1L)
+                .email("x@naver.com")
+                .uid("x")
+                .gender("Male")
+                .joinDate(LocalDate.now())
+                .nickname("kim")
+                .imagePath("x.png")
+                .build();
+
+        Calendar calendar = Calendar.builder().id(1L).build();
+
+        //가짜 객체에 특정 함수를 호출했을 때 특정 객체를 반환
+        //userRepo.findById("x")를 호출한 경우 userJpaEntity를 반환
+        Mockito.when(userRepo.findById("x")).thenReturn(userJpaEntity);
+        Mockito.when(calendarService.getById(1L)).thenReturn(calendar);
+
+        //when
+        User user = userService.getUserById("x");
+
+        //then
+        assertThat(user.getUid()).isEqualTo("x");
+        assertThat(user.getBirth()).isEqualTo(LocalDate.of(2001, 11, 11));
+        assertThat(user.getEmail()).isEqualTo("x@naver.com");
+        assertThat(user.getGender()).isEqualTo("Male");
+        assertThat(user.getJoinDate()).isEqualTo(LocalDate.now());
+        assertThat(user.getNickname()).isEqualTo("kim");
+
+        assertThat(user.getMyCalendar().getId()).isEqualTo(calendar.getId());
+    }
+
+
+    @Test
+    @DisplayName("유저 이메일 조회")
+    void getUserByEmail(){
+        //given
+        UserJpaEntity userJpaEntity = UserJpaEntity.builder()
+                .birth(LocalDate.of(2001, 11, 11))
+                .calendarId(1L)
+                .email("x@naver.com")
+                .uid("x")
+                .gender("Male")
+                .joinDate(LocalDate.now())
+                .nickname("kim")
+                .imagePath("x.png")
+                .build();
+
+        Calendar calendar = Calendar.builder().id(1L).build();
+
+        //가짜 객체에 특정 함수를 호출했을 때 특정 객체를 반환
+        //userRepo.findById("x")를 호출한 경우 userJpaEntity를 반환
+        Mockito.when(userRepo.findByEmail("x@naver.com")).thenReturn(userJpaEntity);
+        Mockito.when(calendarService.getById(1L)).thenReturn(calendar);
+
+        //when
+        User user = userService.getUserByEmail("x@naver.com");
+
+        //then
+        assertThat(user.getUid()).isEqualTo("x");
+        assertThat(user.getBirth()).isEqualTo(LocalDate.of(2001, 11, 11));
+        assertThat(user.getEmail()).isEqualTo("x@naver.com");
+        assertThat(user.getGender()).isEqualTo("Male");
+        assertThat(user.getJoinDate()).isEqualTo(LocalDate.now());
+        assertThat(user.getNickname()).isEqualTo("kim");
+
+        assertThat(user.getMyCalendar().getId()).isEqualTo(calendar.getId());
+    }
+
+
+    @Test
+    @DisplayName("존재하지 않는 유저 이메일 조회시 예외")
+    void getUserByEmailFail(){
+        //given
+
+        //가짜 객체에 특정 함수를 호출했을 때 특정 객체를 반환
+        Mockito.when(userRepo.findByEmail("none@naver.com")).thenThrow(NullPointerException.class);
+
+        //when, then
+        assertThatThrownBy(() ->
+                userService.getUserByEmail("none@naver.com")
+                ).isInstanceOf(NullPointerException.class);
+    }
+
+    @Test
+    @DisplayName("그룹 안에 유저들 조회")
+    void getByGid(){
+        //given
+        Long gid = 1L;
+
+        UserJpaEntity userJpaEntity1 = UserJpaEntity.builder()
+                .birth(LocalDate.of(2001, 11, 11))
+                .calendarId(1L)
+                .email("x1@naver.com")
+                .uid("x1")
+                .gender("Male")
+                .joinDate(LocalDate.now())
+                .nickname("kim")
+                .imagePath("x.png")
+                .build();
+
+        UserJpaEntity userJpaEntity2 = UserJpaEntity.builder()
+                .birth(LocalDate.of(2001, 11, 11))
+                .calendarId(2L)
+                .email("x2@naver.com")
+                .uid("x2")
+                .gender("Male")
+                .joinDate(LocalDate.now())
+                .nickname("kim")
+                .imagePath("x.png")
+                .build();
+
+        Calendar calendar1 = Calendar.builder().id(1L).build();
+        Calendar calendar2 = Calendar.builder().id(2L).build();
+
+        //임의로 group1에 user1, user2가 들어 있는 데이터를 생성
+        GroupAndUserJpaEntity groupAndUserJpaEntity1 = GroupAndUserJpaEntity.builder().gid(gid).uid(userJpaEntity1.getUid()).build();
+        GroupAndUserJpaEntity groupAndUserJpaEntity2 = GroupAndUserJpaEntity.builder().gid(gid).uid(userJpaEntity2.getUid()).build();
+
+
+        //가짜 객체에 특정 함수를 호출했을 때 특정 객체를 반환
+        //userRepo.findById("x")를 호출한 경우 userJpaEntity를 반환
+        Mockito.when(userRepo.findById("x1")).thenReturn(userJpaEntity1);
+        Mockito.when(userRepo.findById("x2")).thenReturn(userJpaEntity1);
+
+        Mockito.when(calendarService.getById(1L)).thenReturn(calendar1);
+        Mockito.when(calendarService.getById(2L)).thenReturn(calendar2);
+
+        Mockito.when(groupAndUserRepo.findByGid(gid)).thenReturn(List.of(new GroupAndUserJpaEntity[]{groupAndUserJpaEntity2, groupAndUserJpaEntity1}));
+
+        //when
+        List<User> users = userService.getByGid(gid);
+
+        //then
+        assertThat(users.size()).isEqualTo(2);
+    }
+
+    @Test
+    @DisplayName("유저 도메인 저장")
+    void save(){
+        //given
+        Calendar calendar = Calendar.builder().id(1L).build();
+        User user = User.builder()
+                .myCalendar(calendar)
+                .birth(LocalDate.of(2001, 11, 11))
+                .email("x@naver.com")
+                .uid("x")
+                .gender("Male")
+                .joinDate(LocalDate.now())
+                .nickname("kim")
+                .build();
+
+        Mockito.when(userRepo.save(UserJpaEntity.from(user))).thenReturn(UserJpaEntity.from(user));
+        //when
+        User result = userService.save(user);
+
+        //then
+        assertThat(result.getMyCalendar().getId()).isEqualTo(calendar.getId());
+        assertThat(result.getBirth()).isEqualTo(LocalDate.of(2001, 11, 11));
+        assertThat(result.getEmail()).isEqualTo("x@naver.com");
+        assertThat(result.getNickname()).isEqualTo("kim");
+        assertThat(result.getMyCalendar().getId()).isEqualTo(calendar.getId());
+    }
+
+}
